9f2d918a69a6d1e7e6e4f267018f82d0ff78af6d
INTEL <yousufzubair1234@gmail.com>
2022-11-14 22:11:53 +0500

    Completed a challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+    const [name, setName] = useState("")
+    const [isMouseOver, setMouseOver] = useState(false);
+
+    function handleChange(event) {
+        let textValue = event.target.value;
+        if (textValue.length < 15){
+            setName(textValue);
+        }
+    }
+
+    function changeColor() {
+        setMouseOver(true)
+    }
+
+    function colorChange() {
+        setMouseOver(false)
+    }
+
+    return (
+        <div className="container">
+            <h1>Hello {name}</h1>
+            <input onChange={handleChange} type="text" placeholder="What's your name?" value={name}/>
+            <button style={{ backgroundColor: isMouseOver ? "black" : "white" }} onMouseOver={changeColor} onMouseOut={colorChange}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
