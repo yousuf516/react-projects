158a94d8467626f7c3864110f083e4408a021cf9
INTEL <yousufzubair1234@gmail.com>
2022-11-15 22:38:58 +0500

    Added a email field as a challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,74 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [fullName, setFullName] = React.useState({
-    fName: "",
-    lName: ""
-  });
-
-  const [isMouseOver, setMouseOver] = React.useState(false);
-
-  function fullNameChange(event) {
-    const {value, name} = event.target;
-
-    setFullName(prevValue => {
-        if(name === "fName"){
-            if(value.length < 10) {
-                return {
-                    fName: value,
-                    lName:prevValue.lName
-                }
-            }
-            else {
-                return {
-                    fName: value.slice(0, 10),
-                    lName:prevValue.lName
-                }   
-            }
-        }
-        else if(name === "lName") {
-            if(value.length < 10) {
-                return {
-                    fName: prevValue.fName,
-                    lName: value
-                }
-            }
-            else {
-                return {
-                    fName: prevValue.fName,
-                    lName: value.slice(0, 10)
-                }
-            }
-        }
-    })
-
-  }
-
-
   function changeColor() {
     setMouseOver(true);
   }
-
   function colorChange() {
     setMouseOver(false);
   }
+  const [isMouseOver, setMouseOver] = React.useState(false);
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function credentialsChange(event) {
+    const { value, name } = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        if (value.length < 10) {
+          return {
+            fName: value,
+            lName: prevValue.lName,
+            email: prevValue.email
+          };
+        } else {
+          return {
+            fName: value.slice(0, 10),
+            lName: prevValue.lName,
+            email: prevValue.email
+          };
+        }
+      } else if (name === "lName") {
+        if (value.length < 10) {
+          return {
+            fName: prevValue.fName,
+            lName: value,
+            email: prevValue.email
+          };
+        } else {
+          return {
+            fName: prevValue.fName,
+            lName: value.slice(0, 10),
+            email: prevValue.email
+          };
+        }
+      } else if (name === "email") {
+        if (value.length < 20) {
+          return {
+            fName: prevValue.fName,
+            lName: prevValue.lName,
+            email: value
+          };
+        } else {
+          return {
+            fName: prevValue.fName,
+            lName: prevValue.lName,
+            email: value.slice(0, 30)
+          };
+        }
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello {fullName.fName + " " + fullName.lName}</h1>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
       <form>
         <input
-          onChange={fullNameChange}
+          onChange={credentialsChange}
           type="text"
           placeholder="First Name"
-          value={fullName.fName}
+          value={contact.fName}
           name="fName"
         />
-        <input 
-          onChange={fullNameChange} 
-          type="text" 
-          name="lName" 
-          placeholder="Last Name" 
-          value={fullName.lName}  
+        <input
+          onChange={credentialsChange}
+          type="text"
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={credentialsChange}
+          type="email"
+          value={contact.email}
+          name="email"
+          placeholder="Email"
         />
         <button
           type="submit"
