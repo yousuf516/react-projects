71d1632385fdfb9ea1000748396a72189c9cef2d
INTEL <yousufzubair1234@gmail.com>
2022-11-15 17:01:04 +0500

    Completed the challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,49 @@
 import React from "react";
 
 function App() {
-  const [fName, setfName] = React.useState("");
+  const [fullName, setFullName] = React.useState({
+    fName: "",
+    lName: ""
+  });
+
   const [isMouseOver, setMouseOver] = React.useState(false);
 
-  function handleChange(event) {
-    let textValue = event.target.value;
-    if (textValue.length < 15) {
-        setfName(textValue);
-    }
+  function fullNameChange(event) {
+    const {value, name} = event.target;
+
+    setFullName(prevValue => {
+        if(name === "fName"){
+            if(value.length < 10) {
+                return {
+                    fName: value,
+                    lName:prevValue.lName
+                }
+            }
+            else {
+                return {
+                    fName: value.slice(0, 10),
+                    lName:prevValue.lName
+                }   
+            }
+        }
+        else if(name === "lName") {
+            if(value.length < 10) {
+                return {
+                    fName: prevValue.fName,
+                    lName: value
+                }
+            }
+            else {
+                return {
+                    fName: prevValue.fName,
+                    lName: value.slice(0, 10)
+                }
+            }
+        }
+    })
+
   }
+
 
   function changeColor() {
     setMouseOver(true);
@@ -20,15 +54,22 @@ function App() {
   }
   return (
     <div className="container">
-      <h1>Hello {fName}</h1>
+      <h1>Hello {fullName.fName + " " + fullName.lName}</h1>
       <form>
         <input
-          onChange={handleChange}
+          onChange={fullNameChange}
           type="text"
-          placeholder="What's your name?"
-          value={fName}
+          placeholder="First Name"
+          value={fullName.fName}
+          name="fName"
         />
-        <input name="lName" placeholder="Last Name" />
+        <input 
+          onChange={fullNameChange} 
+          type="text" 
+          name="lName" 
+          placeholder="Last Name" 
+          value={fullName.lName}  
+        />
         <button
           type="submit"
           style={{ backgroundColor: isMouseOver ? "black" : "white" }}
